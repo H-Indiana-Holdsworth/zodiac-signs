4b01bc6925347ac36b27f216cefac598d4f4e54e
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-11-30 13:10:52 -0700

    Adds background

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,21 +2,8 @@ import React from 'react'
 import './Main.css'
 import background from '../../background.png'
 import { animals } from '../../data'
-import Animal from '../Animal/Animal.js'
+import { zodiac } from '../../data'
 
 export default function Main() {
-  return (
-    <main className="background" style={{ backgroundImage: `url(${background})` }}>
-      {animals.map((animal) => (
-        <Animal
-          key={animal.name}
-          name={animal.name}
-          type={animal.type}
-          says={animal.says}
-          left={animal.left}
-          top={animal.top}
-        />
-      ))}
-    </main>
-  )
+  return <main className="background" style={{ backgroundImage: `url(${background})` }}></main>
 }
